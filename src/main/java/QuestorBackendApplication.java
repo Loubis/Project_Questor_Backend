306b93bf0876da2e094526e012ebf80c61284de5
ADESSO\kersting <lukaspulle@gmail.com>
2018-07-21 11:30:00 +0200

    Spring Boot init push

---
+++ src/main/java/QuestorBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestorBackendApplication.class, args);
+	}
+}
